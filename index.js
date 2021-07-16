8ddab467e2c50b6afbcd8621db3fef9f79210bf2
danger2123 <sellmysolenyc@gmail.com>
2021-07-16 04:05:29 -0400

    added local storage saving

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
 const toDoInput = document.getElementById("to-do-input");
-const toDoBtn = document.getElementById("to-do-btn");
+const addBtn = document.getElementById("add-btn");
 const toDoContainer = document.getElementById("to-do-list-container-2");
 const toDoList = document.getElementById("to-do-list");
-const toDoFromLocalStorage = JSON.parse(localStorage.getItem("toDo"));
+const toDoLocalStorage = JSON.parse(localStorage.getItem("toDo"));
 const deleteBtn = document.getElementById("delete-btn");
+const saveBtn = document.getElementById("save-btn");
 let toDoEntryNum = 0;
+let toDoEntry = [];
 
-// localStorage.clear();
+if (toDoLocalStorage) {
+  toDoContainer.setAttribute("class", `box container-2`);
+  deleteBtn.removeAttribute("style");
+  toDoEntry = toDoLocalStorage;
+  render(toDoEntry);
+}
 
-// if (toDoFromLocalStorage) {
-//   toDoContainer.setAttribute("class", `box container-2`);
-//   myToDo = toDoFromLocalStorage;
-//   render(myToDo);
-// }
-
-toDoBtn.addEventListener("click", function () {
-  let toDoEntry = [];
+addBtn.addEventListener("click", function () {
   toDoEntryNum += 1;
   if (toDoEntryNum > 0) {
     toDoContainer.setAttribute("class", `box container-2`);
     deleteBtn.removeAttribute("style");
   }
   toDoEntry.push(toDoInput.value);
-  localStorage.setItem("toDo", JSON.stringify(toDoEntry));
   toDoInput.value = "";
-  toDoList.innerHTML += `
-  <li>${toDoEntryNum}. ${toDoEntry}<i class="fas fa-trash-alt"></i></li>
-    `;
+  localStorage.setItem("toDo", JSON.stringify(toDoEntry));
+  render(toDoEntry);
 });
 
 deleteBtn.addEventListener("click", function () {
   localStorage.clear();
   toDoList.textContent = "";
   toDoEntryNum = 0;
+  toDoEntry = [];
   deleteBtn.setAttribute("style", "display: none");
-  if (toDoEntryNum === 0) {
-    toDoContainer.removeAttribute("class");
-  }
+  toDoContainer.removeAttribute("class");
 });
 
-// function render(items) {
-//   let listItems = [];
-//   for (let i = 0; i < items.length; i++) {
-//     listItems += `<li>${toDoEntryNum}. ${items[i]}<i class="fas fa-trash-alt"></i></li>`;
-//     toDoList.innerHTML += listItems;
-//   }
-// }
+function render(items) {
+  let listItems = [];
+  for (let i = 0; i < items.length; i++) {
+    listItems += `<li>${items[i]}<i class="fas fa-trash-alt"></i></li>`;
+    toDoList.innerHTML = listItems;
+  }
+}
