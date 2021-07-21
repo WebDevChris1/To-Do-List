5e93d19dcd683e6a600c08e66b0ee9cbfb526862
danger2123 <sellmysolenyc@gmail.com>
2021-07-21 10:15:04 -0400

    added total task counter

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const toDoList = document.getElementById("to-do-list");
 const toDoLocalStorage = JSON.parse(localStorage.getItem("toDo"));
 const deleteBtn = document.getElementById("delete-btn");
 const saveBtn = document.getElementById("save-btn");
+const total = document.getElementById("total");
 let toDoEntryNum = 0;
 let toDoEntry = [];
 
@@ -12,6 +13,7 @@ if (toDoLocalStorage) {
   toDoContainer.setAttribute("class", `box container-2`);
   deleteBtn.removeAttribute("style");
   toDoEntry = toDoLocalStorage;
+  totalTaskCount(toDoEntry);
   render(toDoEntry);
 }
 
@@ -35,6 +37,7 @@ deleteBtn.addEventListener("click", function () {
   entryId = [];
   deleteBtn.setAttribute("style", "display: none");
   toDoContainer.removeAttribute("class");
+  totalTaskCount(toDoEntry);
 });
 
 function render(items) {
@@ -43,6 +46,7 @@ function render(items) {
   for (let i = 0; i < items.length; i++) {
     listItems += `<li id="${itemId}">${items[i]}<button onClick="removeTask(${itemId})"><i class="fas fa-trash-alt"></i></button></li>`;
     toDoList.innerHTML = listItems;
+    totalTaskCount(toDoEntry);
     itemId += 1;
   }
 }
@@ -54,12 +58,21 @@ function removeTask(itemId) {
   toDoEntry = [];
   for (var i = 0; i < lisItems.length; ++i) {
     toDoEntry.push(lisItems[i].textContent);
-    console.log(toDoEntry);
     localStorage.setItem("toDo", JSON.stringify(toDoEntry));
   }
+  totalTaskCount(toDoEntry);
+  // if (toDoEntry > 1) {
+  //   total.removeAttribute("style");
+  // }
   if (toDoEntry.length === 0) {
     localStorage.clear();
     deleteBtn.setAttribute("style", "display: none");
     toDoContainer.removeAttribute("class");
+    // total.setAttribute("style", "display: none");
   }
+}
+
+function totalTaskCount(entry) {
+  let totalTask = "Total: " + entry.length;
+  total.textContent = totalTask;
 }
